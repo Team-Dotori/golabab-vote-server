170cf6ded3836b7970b006de3e70b0fd8f62d815
지인호 <77221262+key-del-jeeinho@users.noreply.github.com>
2021-09-11 16:32:49 +0900

    Spring boot 및 Gradle environment 구성

---
+++ src/main/java/com/dotori/golababvoteserver/GolababVoteServerApplication.java
@@ -0,0 +1,13 @@
+package com.dotori.golababvoteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolababVoteServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolababVoteServerApplication.class, args);
+	}
+
+}
