34c1152a78e56949a4257c7267db398286ae1d79
YongWoo <aba1430@naver.com>
2021-09-15 21:56:49 +0900

    [ADD] 메세지 추가 기능 구현 완료

---
+++ src/main/java/com/dotori/golababvoteserver/Service/TipticService.java
@@ -0,0 +1,30 @@
+package com.dotori.golababvoteserver.Service;
+
+import com.dotori.golababvoteserver.dto.MessageDto;
+import com.dotori.golababvoteserver.model.ImproveMessage;
+import com.dotori.golababvoteserver.repository.TipticRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+@Service
+public class TipticService {
+    private final TipticRepository tipticRepository;
+
+    public void MessageSave(MessageDto messageDto){
+        if (messageDto != null ){
+            throw new NullPointerException();
+        }
+        LocalDate date = LocalDate.now();
+        tipticRepository.save(toEntity(messageDto,date));
+    }
+
+    public ImproveMessage toEntity(MessageDto messageDto, LocalDate date){
+        return ImproveMessage.builder()
+                .message(messageDto.getMessage())
+                .date(date)
+                .build();
+    }
+}
