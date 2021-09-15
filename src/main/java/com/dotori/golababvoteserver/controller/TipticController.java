627109969b6bb2162406eac07edd781dd9ba3e8b
YongWoo <aba1430@naver.com>
2021-09-15 21:15:13 +0900

    [ADD] Create TipticController

---
+++ src/main/java/com/dotori/golababvoteserver/controller/TipticController.java
@@ -0,0 +1,21 @@
+package com.dotori.golababvoteserver.controller;
+
+import com.dotori.golababvoteserver.mealDto.MessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/tiptic")
+public class TipticController {
+
+    @PostMapping("/add-improve-message")                              //추후 상세 Exception으로 변경
+    public void improveMessageSave(@RequestBody MessageDto messageDto) throws Exception {
+
+    }
+}
