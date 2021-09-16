50d8e418dc62b33526d0215c2eb2ddf1ed93c7d1
YongWoo <aba1430@naver.com>
2021-09-16 10:28:05 +0900

    [ADD] Create GetMessage Service

--- src/main/java/com/dotori/golababvoteserver/service/TipticService.java
+++ src/main/java/com/dotori/golababvoteserver/service/TipticService.java
@@ -6,7 +6,8 @@ import com.dotori.golababvoteserver.repository.TipticRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -21,9 +22,26 @@ public class TipticService {
         tipticRepository.save(toEntity(messageDto,date));
     }
 
-    public String GetMessage(){
-
+    public List<String> GetMessage(){
+        Date cd = new Date();
+        Date bef5 = before5Days(cd);
+        
+        List<String> tiptics = tipticRepository.getImproveMessageByDateAfter(bef5);
+        int numOfTiptics = tiptics.size(); //20
+        int[] randomIdx = new int[7]; //5 랜덤 < numOfTiptics
+        List<String> random5 = Arrays.asList(randomIdx).stream()
+                .map(el -> tiptics.get(new Random().nextInt(numOfTiptics))).collect(Collectors.toList());
+        return random5;
     }
+
+    public Date before5Days(Date date){
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE,-5);
+        date.setTime(cal.getTimeInMillis());
+        return date;
+    }
+
 
     public ImproveMessage toEntity(MessageDto messageDto, Date date){
         return ImproveMessage.builder()
