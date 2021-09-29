83f0e474ac06304128bc397d200f4b7526059c3f
YongWoo <aba1430@naver.com>
2021-09-29 16:17:18 +0900

    [ADD] WebMvcConfig

---
+++ src/main/java/com/dotori/golababvoteserver/security/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.dotori.golababvoteserver.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(loggingInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/vendor/**", "/css/*", "/img/*");
+    }
+}
